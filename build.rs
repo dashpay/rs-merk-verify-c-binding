9536f94facb9d65ab254907cf13e82b5801fd39f
Quantum Explorer <quantum@dash.org>
2021-07-22 13:24:01 +0700

    fixes

--- build.rs
+++ build.rs
@@ -3,10 +3,10 @@ extern crate cbindgen;
 use std::env;
 
 fn main() {
-    // let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    // let mut config: cbindgen::Config = Default::default();
-    // config.language = cbindgen::Language::C;
-    // cbindgen::generate_with_config(&crate_dir, config)
-    //     .unwrap()
-    //     .write_to_file("target/example.h");
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let mut config: cbindgen::Config = Default::default();
+    config.language = cbindgen::Language::C;
+    cbindgen::generate_with_config(&crate_dir, config)
+        .unwrap()
+        .write_to_file("target/merk.h");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,49 @@
 use merk::execute_proof;
-use merk::proofs::query::Map;
 use std::slice;
-
 use std::ptr;
-
 #[repr(C)]
 pub struct Element {
-    pub key: *const u8, //32 bytes
-    pub bool: bool, //1 byte
-    pub value_length: usize, //8 bytes
+    pub key: *const u8,
+    //32 bytes
+    pub bool: bool,
+    //1 byte
+    pub value_length: usize,
+    //8 bytes
     pub value: *const u8, //value_length bytes
 }
-
 #[repr(C)]
 pub struct ExecuteProofResult {
     pub hash: *const u8,  //32 bytes
     pub element_count: usize,  //8 bytes
-    pub elements: *const Element, //sizeof(pointer)
+    pub elements: *const *mut Element, //sizeof(pointer)
 }
 
 #[no_mangle]
-pub extern "C" fn execute_proof_c(c_array: *const u8, length: usize) -> *const ExecuteProofResult {
-    let rust_array: &[u8] = unsafe { slice::from_raw_parts(c_array, length as usize) };
+pub extern "C" fn execute_proof_c(c_array: *const u8) -> *const ExecuteProofResult {
+    let rust_array: &[u8] = unsafe { slice::from_raw_parts(c_array, 32 as usize) };
     let execute_proof_result = execute_proof(rust_array);
     match execute_proof_result {
         Err(_) => ptr::null(),
         Ok((hash, map)) => {
-            let elements_map = map.all().map(|key_value_pair| -> Element {
+            let elements_map = map.all().map(|key_value_pair| {
                 let element_hash = key_value_pair.0;
                 let (exists, value) = key_value_pair.1;
-                Element {
+                let e = Element {
                     key: element_hash.as_ptr(),
                     bool: *exists,
                     value_length: 0,
                     value: value.as_ptr(),
-                }
+                };
+                Box::into_raw(Box::new(e))
             });
-            let elements = elements_map.collect();
             let elements_count = elements_map.len();
+            let a: Vec<*mut Element> = elements_map.collect();
             let result = ExecuteProofResult {
                 hash: hash.as_ptr(),
                 element_count: elements_count,
-                elements,
+                elements: a.as_ptr(),
             };
-            result
+            Box::into_raw(Box::new(result))
         }
     }
-
 }
