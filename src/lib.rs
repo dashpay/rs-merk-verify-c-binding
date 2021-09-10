c5a9d67840a88325030ea69f5602098b76a8e060
pankcuf <rastacake@gmail.com>
2021-09-10 16:11:34 +0300

    [impl ffi method for proper destroying memory & therefore to avoid memory leaks]

--- src/lib.rs
+++ src/lib.rs
@@ -12,42 +12,47 @@ pub struct Element {
 }
 #[repr(C)]
 pub struct ExecuteProofResult {
-    pub hash: *const [u8; 32],  //32 bytes
+    pub hash: *mut [u8; 32],  //32 bytes
     pub element_count: usize,  //8 bytes
     pub elements: *mut *mut Element, //sizeof(pointer)
 }
 #[no_mangle]
-pub extern "C" fn execute_proof_c(c_array: *const u8, length: usize) -> *const ExecuteProofResult {
+pub extern fn execute_proof_c(c_array: *const u8, length: usize) -> *mut ExecuteProofResult {
     let rust_array: &[u8] = unsafe { slice::from_raw_parts(c_array, length as usize) };
     let execute_proof_result = execute_proof(rust_array);
     match execute_proof_result {
-        Err(_) => ptr::null(),
+        Err(_) => ptr::null_mut(),
         Ok((hash, map)) => {
             let elements_map = map.all().map(|(key, (exists, value))| {
-                let key_copy = key.to_vec();
-                let value_copy = value.to_vec();
-                let mut key_slice = key_copy.into_boxed_slice();
-                let mut value_slice = value_copy.into_boxed_slice();
-                let e = Element {
+                let mut key_slice = key.to_vec().into_boxed_slice();
+                let mut value_slice = value.to_vec().into_boxed_slice();
+                let element = Element {
                     key_length: key_slice.len(),
                     key: key_slice.as_mut_ptr(),
                     bool: *exists,
                     value_length: value_slice.len(),
-                    value: value_slice.as_mut_ptr(),
+                    value: value_slice.as_mut_ptr()
                 };
-                mem::forget(key_slice);
-                mem::forget(value_slice);
-                Box::into_raw(Box::new(e))
+                Box::into_raw(Box::new(element))
             });
             let a:Vec<*mut Element> = elements_map.collect();
             let mut elements_slice = a.into_boxed_slice();
             let result = ExecuteProofResult {
                 hash: Box::into_raw(Box::new(hash)),
                 element_count: elements_slice.len(),
-                elements: elements_slice.as_mut_ptr(),
+                elements: elements_slice.as_mut_ptr()
             };
             mem::forget(elements_slice);
             Box::into_raw(Box::new(result))
         }
+    }
+}
+#[no_mangle]
+pub unsafe extern fn destroy_proof_c(proof_result: *mut ExecuteProofResult) {
+    let result = Box::from_raw(proof_result);
+    let _ = Box::from_raw(result.hash);
+    let vec = Vec::from_raw_parts(result.elements, result.element_count, result.element_count);
+    for &x in vec.iter() {
+        let _ = Box::from_raw(x);
     }
 }
